abe86b35feff69ff090637f02a7474faf6c4fade
Brian0317 <onedev975@gmail.com>
2023-02-10 05:17:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Realtime Video Chat Application
+
+## Introduction
+
+ I have build and deploy a React Video Chat Application using WebRTC.
+
+Setup:
+
+- run `npm i && npm start` for both client and server side to start the development server
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import VideoPlayer from "./components/VideoPlayer.jsx";
+import Options from "./components/Options.jsx";
+import Notifications from "./components/Notifications.jsx";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
+};
+
+export default App;
